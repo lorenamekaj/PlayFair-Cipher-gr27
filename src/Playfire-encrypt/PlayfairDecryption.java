b9f4c0d8f5805fe0bd7080de991b5ddb00a06daa
MajlindaHalilaj <121357662+MajlindaHalilaj@users.noreply.github.com>
2023-04-15 16:40:46 +0200

    Constructing and returning the plaintext

--- src/Playfire-encrypt/PlayfairDecryption.java
+++ src/Playfire-encrypt/PlayfairDecryption.java
@@ -67,10 +67,46 @@ public class PlayfairDecryption {
                     }
                 }
             }
+            if (row1 == -1 || col1 == -1 || row2 == -1 || col2 == -1) {
+                throw new RuntimeException("Invalid ciphertext characters: " + c1 + c2);
+            }
+
+            if (row1 == row2) {
+                col1 = (col1 - 1 + 5) % 5;
+                col2 = (col2 - 1 + 5) % 5;
+            } else if (col1 == col2) {
+                row1 = (row1 - 1 + 5) % 5;
+                row2 = (row2 - 1 + 5) % 5;
+            } else {
+                int tmp = col1;
+                col1 = col2;
+                col2 = tmp;
+            }
+
+            plaintext.append(keyMatrix[row1][col1]);
+            plaintext.append(keyMatrix[row2][col2]);
+        }
+
+        int m = 0;
+        while (m < plaintext.length() - 2) {
+            char c1 = plaintext.charAt(m);
+            char c2 = plaintext.charAt(m + 1);
+            char c3 = plaintext.charAt(m + 2);
+
+            if (c1 == c3 && c2 == 'X') {
+                plaintext.deleteCharAt(m + 1);
+                m++;
+                continue;
+            }
+
+            m++;
+        }
+        return plaintext.toString();
+    }
 
 
 
-            public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the key: ");
         String key = scanner.nextLine();
